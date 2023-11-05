604b4fa2bf70049d9a3d9a1a2f03eaa435914187
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-05 20:25:30 -0300

    Curso Java - Creacion de Metodos Cap.49

---
+++ src/operaciones/Aritmetica.java
@@ -0,0 +1,19 @@
+package operaciones;
+
+/**
+ *
+ * @author gabri
+ */
+public class Aritmetica {
+
+    //Atributos de la clase
+    int a;
+    int b;
+
+    //Metodo
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
